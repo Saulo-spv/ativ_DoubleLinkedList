992240c7081f6f8cc8eb12f7b08089ae8add3adc
Saulo Pires <saulopiresvieira5@gmail.com>
2024-04-24 19:57:07 -0300

    Adicione funções pedidas

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -10,13 +10,13 @@ typedef struct Node {
     Node* ptrPrev;
 } Node;
 
-
 Node* newNode(int);
 void addElementEnd(Node**, int);
 void showList(Node*);
 void insertBefore(Node*, int);
+void deleteNode(Node**, Node*);
 void deleteNodebyValue(Node**, int);
-Node* searchNodebyValue(Node**, int);
+Node* searchNodebyValue(Node*, int);
 
 
 int main() {
@@ -31,9 +31,25 @@ int main() {
     cout << "Lista Original:" << endl;
     showList(head);
 
+    // Procurando um nó com valor 20
+    Node* node20 = searchNodebyValue(head, 20);
+    if (node20 != nullptr) {
+        cout << "\nNó com valor 20 encontrado." << endl;
+    }
+
+    // Inserindo um nó antes do nó com valor 20
+    if (node20 != nullptr) {
+        cout << "\nInserindo 15 antes do nó com valor 20:" << endl;
+        insertBefore(node20, 15);
+        showList(head);
+    }
+
+    // Deletando o nó com valor 20
+    cout << "\nDeletando o nó com valor 20:" << endl;
+    deleteNodebyValue(&head, 20);
+    showList(head);
     return 0;
 }
-
 
 Node* newNode(int iPayload) {
     Node* temp = (Node*) malloc(sizeof(Node));
@@ -72,4 +88,63 @@ void showList(Node* head) {
             current = current->ptrNext;
         } 
     }
+}
+
+Node* searchNodebyValue(Node* head, int iValue)
+{
+    if (head == nullptr) {
+        cout << "Lista Vazia!." << endl;
+        return head;
+    } else {
+        // Criamos um nó currente a avançamos até encontrar o nó buscado
+        Node* current = head;
+        while (current != nullptr && current->iValue != iValue) current = current->ptrNext;
+        
+        // Verificamos se o nó desejado foi encontrado
+        if (current == nullptr) {
+            cout << "Valor não encontrado." << endl;
+            return current;
+        } else {
+            return current;
+        }
+    }
+}
+
+void insertBefore(Node* position, int iValue){
+    if(position == nullptr){
+        cout << "Impossivel inserir nessa posição!" << endl;
+        return;
+    }
+
+    Node* node = newNode(iValue);
+
+    // Insere o nó na posição desejada
+    if (position->ptrPrev == nullptr) {
+        position->ptrPrev = node;
+        node->ptrNext = position;
+    } else{
+        Node* temp = position->ptrPrev;
+        temp->ptrNext = node;
+        node->ptrPrev = temp;
+        node->ptrNext = position;
+        position->ptrPrev = node;
+    }
+}
+
+void deleteNode(Node** head, Node* node) {
+    if ((*head) == NULL || node == NULL){
+        cout << "Lista ou ponteiro vazio." << endl;
+        return;
+    }
+
+    // Atualiza os ponteiros e exclui o no desejado
+    if ((*head) == node) (*head) = node->ptrNext;
+    if (node->ptrNext != NULL) node->ptrNext->ptrPrev = node->ptrPrev;
+    if (node->ptrPrev != NULL) node->ptrPrev->ptrNext = node->ptrNext;
+    free(node);
+}
+
+void deleteNodebyValue(Node** head, int iValue) {
+    Node* node = searchNodebyValue(*head, iValue);
+    deleteNode(head, node);
 }
